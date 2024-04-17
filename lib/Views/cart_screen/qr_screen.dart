24d601cc5e2f7d974ca6d20a55bf9ff85d1a9694
Tung\P <peerapat.jakk@bumail.net>
2024-04-17 17:06:02 +0700

    กดหน้า brand ไป store_screen

--- lib/Views/cart_screen/qr_screen.dart
+++ lib/Views/cart_screen/qr_screen.dart
@@ -72,6 +72,123 @@ class _QRScreenState extends State<QRScreen> {
     }
   }
 
+  createChargeWithKbank() async {
+    String secretKey = 'skey_test_5yzhwpoh5cu85yb4qrr';
+    String urlAPI = 'https://api.omise.co/charges';
+
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$secretKey:'));
+
+    Map<String, String> headerMap = {};
+    headerMap['authorization'] = basicAuth;
+    headerMap['Cache-Control'] = 'no-cache';
+    headerMap['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    Map<String, dynamic> data = {};
+    data['amount'] = controller.totalP.value.toString();
+    data['currency'] = 'thb';
+    data['source[type]'] = 'mobile_banking_kbank';
+    data['return_uri'] = 'com.example.flutter.finalproject://login-callback';
+
+    Uri uri = Uri.parse(urlAPI);
+
+    http.Response response = await http.post(
+      uri,
+      headers: headerMap,
+      body: data,
+    );
+
+      var resultCharge = jsonDecode(response.body);
+      print('status Kbank ===> ${resultCharge['status']}');
+
+      await checkStatus(resultCharge['id'], context);
+    }
+
+Future<void> checkStatus(String chargeId, BuildContext context) async {
+  while (true) {
+    await Future.delayed(Duration(seconds: 5)); // ตรวจสอบทุก 5 วินาที
+
+    String secretKey = 'skey_test_5yzhwpoh5cu85yb4qrr';
+    String urlAPI = 'https://api.omise.co/charges/$chargeId';
+
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$secretKey:'));
+
+    Map<String, String> headerMap = {};
+    headerMap['authorization'] = basicAuth;
+    headerMap['Cache-Control'] = 'no-cache';
+    headerMap['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    Uri uri = Uri.parse(urlAPI);
+
+    http.Response response = await http.get(uri, headers: headerMap);
+
+    var resultCharge = jsonDecode(response.body);
+    print('status Kbank ===> ${resultCharge['status']}');
+    print(resultCharge);
+
+    if (resultCharge['status'] == 'successful') {
+      _showSuccessDialog(context);
+      break; // หยุดตรวจสอบเมื่อชำระเงินสำเร็จ
+    } else if (resultCharge['status'] == 'failed') {
+      VxToast.show(context, msg: "failed");
+      break; // หยุดตรวจสอบเมื่อชำระเงินไม่สำเร็จ
+    } else {
+      // หากยังไม่สำเร็จ ทำให้ลูปวนต่อไป
+      continue;
+    }
+  }
+}
+
+Future<void> _showSuccessDialog(BuildContext context) async {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: whiteColor,
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              SizedBox(height: 50),
+              Image.asset('assets/images/Finishpay.PNG'),
+              SizedBox(height: 40),
+              Text(
+                'Payment was successful!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                '${controller.totalP.value} Bath',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+
+  String selectedPaymentMethod = paymentMethods[controller.paymentIndex.value];
+  await controller.placeMyOrder(
+    orderPaymentMethod: selectedPaymentMethod,
+    totalAmount: controller.totalP.value,
+  );
+
+  await controller.clearCart();
+  VxToast.show(context, msg: "Order placed successfully");
+  await Future.delayed(Duration(seconds: 4));
+  Get.find<CartController>().totalP.refresh();
+  Get.offAll(() => MainHome());
+}
+
   @override
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> banks = [
