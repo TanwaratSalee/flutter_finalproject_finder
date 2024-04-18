b2a34b6e9919c4a1febfeed6ab6b512a915e4502
TanwaratSalee <tanwas.17@gmail.com>
2024-04-19 02:35:12 +0700

    Noey update code

--- lib/Views/cart_screen/qr_screen.dart
+++ lib/Views/cart_screen/qr_screen.dart
@@ -1,11 +1,9 @@
 import 'dart:convert'; // แก้ไขบรรทัดนี้
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart'; // เพิ่มเข้ามาเพื่อใช้งาน Material widgets
 import 'package:flutter_finalproject/consts/consts.dart';
+import 'package:flutter_finalproject/consts/lists.dart';
 import 'package:flutter_finalproject/controllers/cart_controller.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -20,8 +18,59 @@ class QRScreen extends StatefulWidget {
 }
 
 class _QRScreenState extends State<QRScreen> {
-  var controller = Get.find<CartController>();
-  String? _selectedBank;
+  var controller = Get.find<CartController>(); // ตัวอย่างการเข้าถึง controller
+  String? downloadUri;
+
+  @override
+  void initState() {
+    super.initState();
+    createChargeWithPromptPay();
+  }
+
+  createChargeWithPromptPay() async {
+    String secretKey = 'skey_test_5yzhwpoh5cu85yb4qrr'; // ใช้ Secret Key ของคุณ
+    String urlAPI = 'https://api.omise.co/charges';
+
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$secretKey:'));
+
+    Map<String, String> headerMap = {};
+    headerMap['authorization'] = basicAuth;
+    headerMap['Cache-Control'] = 'no-cache';
+    headerMap['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    Map<String, dynamic> data = {};
+    data['amount'] = controller.totalP.value.toString();
+    data['currency'] = 'thb';
+    data['source[type]'] = 'promptpay';
+
+    Uri uri = Uri.parse(urlAPI);
+
+    http.Response response = await http.post(
+      uri,
+      headers: headerMap,
+      body: data,
+    );
+
+    if (response.statusCode == 200) {
+      final resultCharge = jsonDecode(response.body);
+      print('Charge created: ${resultCharge['id']}');
+
+      // ตรวจสอบและเข้าถึง download_uri
+      downloadUri =
+          resultCharge['source']?['scannable_code']?['image']?['download_uri'];
+      if (downloadUri != null) {
+        setState(() {
+          downloadUri =
+              resultCharge['source']['scannable_code']['image']['download_uri'];
+          print('Dddddddddd: $downloadUri');
+        });
+      } else {
+        print('Download URI not found');
+      }
+    } else {
+      print('Failed to create charge: ${response.body}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,49 +82,30 @@ class _QRScreenState extends State<QRScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mobile Banking'),
-        leading: BackButton(),
+        title: Text('Display QR Code'),
+        backgroundColor: Colors.white, // สีของ AppBar
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: banks.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Icon(banks[index]['icon']),
-                  title: Text(banks[index]['name']),
-                  trailing: _selectedBank == banks[index]['name']
-                      ? Icon(Icons.check, color: Colors.green)
-                      : null,
-                  onTap: () {
-                    setState(() {
-                      _selectedBank = banks[index]['name'];
-                    });
-                  },
-                );
-              },
+      backgroundColor: Colors.white, // สีพื้นหลังของ Scaffold
+      body: Center(
+        child: Container(
+          width: 300, // ความกว้างของสี่เหลี่ยม
+          height: 200, // ความสูงของสี่เหลี่ยม
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black, // สีของเส้นกรอบ
+              width: 2.0, // ความหนาของเส้นกรอบ
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: _selectedBank != null
-                  ? () {
-                      // ทำการดำเนินการยืนยันเมื่อมีธนาคารถูกเลือก
-                    }
-                  : null,
-              child: Text('Confirm'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor, // สีของปุ่ม
-                minimumSize: Size(double.infinity, 50), // ขนาดของปุ่ม
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // มุมโค้งของปุ่ม
-                ),
-              ),
-            ),
-          ),
-        ],
+          //   child: Center(
+          //     child: downloadUri != null
+          //         ? Image.network(
+          //             downloadUri!,
+          //             width: 150,
+          //             height: 150,
+          //           )
+          //         : Text('No image available'),
+          //   ),
+        ),
       ),
     );
   }
